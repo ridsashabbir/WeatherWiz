0e81ef0513c2bab565e5cd0563afa45624f21a90
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-06-09 19:15:40 +0500

    styling

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=bb97dcf45fb2360d40faab747c1ee7fd`;
 
   return (
-    <div className="App">
+    <div className="app">
       <div className="container">
         <div className="top">
           <div className="location">
@@ -21,12 +21,17 @@ function App() {
 
         <div className="bottom">
           <div className="feels">
-            <h1>60&deg;F</h1>
+            <h1>65&deg;F</h1>
+            <p className="bold">Feels like</p>
           </div>
           <div className="humidity">
-            <p>20%</p>
+            <p>20%</p> &nbsp;
+            <p className="bold">&nbsp; Humidity</p>
           </div>
-          <div className="wind">12 MPH</div>
+          <div className="wind">
+            <p>12 MPH</p> &nbsp;
+            <p className="bold">&nbsp; Wind Speed</p>
+          </div>
         </div>
       </div>
     </div>
