c3972826b281f8110425ccc3bbac2d5741746471
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-06-09 19:05:37 +0500

    basic styling

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,9 +1,34 @@
-import { useState } from "react";
+import React, { useState } from "react";
+// import axios from "axios";
 
 function App() {
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=bb97dcf45fb2360d40faab747c1ee7fd`;
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60&deg;F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels">
+            <h1>60&deg;F</h1>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">12 MPH</div>
+        </div>
+      </div>
     </div>
   );
 }
